d6dedb80c14620117845b9684a67b117f3a089f6
Anuchit Chalothorn <anoochit@gmail.com>
2020-07-11 08:50:26 +0700

    add upload route

--- bin/server.dart
+++ bin/server.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_framework/src/core/server.dart';
 import 'package:angel_hot/angel_hot.dart';
+import 'package:http_parser/src/media_type.dart';
 import 'package:logging/logging.dart';
 
 Future<void> main() async {
   var hot = HotReloader(createServer, [Directory('bin'), Directory('lib')]);
-
   var http = await hot.startServer('0.0.0.0', 3000);
 
   print('${http.address.address}:${http.port}');
@@ -17,7 +19,29 @@ Future<void> main() async {
 Future<Angel> createServer() async {
   var app = Angel();
 
-  app.get('/', (req, res) => 'ok');
+  app.get('/', (req, res) {
+    res.contentType = MediaType.parse('text/json');
+    res.write('{"message":"ok"}');
+  });
+
+  app.get('/api/version', (req, res) {
+    res.contentType = MediaType.parse('text/json');
+    res.write('{"version":0.1}');
+  });
+
+  app.post('/api/upload', (req, res) async {
+    await req.parseBody();
+    var file = req.uploadedFiles.first;
+    if (file == null) {
+      throw AngelHttpException.badRequest(message: 'Bad Request');
+    } else {
+      var directory = Directory.current;
+      var someFile = File('${directory.path}/data/${file.filename}');
+      await file.data.pipe(someFile.openWrite());
+      res.contentType = MediaType.parse('text/json');
+      res.write('{"path":"${someFile.path}", "filename" : "${file.filename}"}');
+    }
+  });
 
   app.fallback((req, res) => throw AngelHttpException.notFound(message: 'not found'));
 
